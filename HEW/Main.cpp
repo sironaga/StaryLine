2f35b9efab2944b046244ffdd5f23ec1162aab04
sironaga <akinosyoe@gmail.com>
2024-10-04 23:19:27 +0900

    DirectX

--- HEW/Main.cpp
+++ HEW/Main.cpp
@@ -2,3 +2,12 @@
 
 
 */
+
+/* Include */
+#include"DirectX.h"
+
+/* */
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	ALLInit(hInstance, nCmdShow);
+}
