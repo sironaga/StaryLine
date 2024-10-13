d7e3011142a602b19b0db591e81d29b235da6f11
sironaga <akinosyoe@gmail.com>
2024-10-13 19:10:00 +0900

    Controller

---
+++ HEW/Controller.cpp
@@ -0,0 +1,13 @@
+/*
+	XInputを使用したControllerの入出力
+	2024/10/13 制作開始　秋野
+*/
+
+/* Include */
+#include<Windows.h>
+#include<Xinput.h>
+#pragma comment(lib, "xinput.lib")
+
+/* 構造体 */
+XINPUT_STATE state;
+
