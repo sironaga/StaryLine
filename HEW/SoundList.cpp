34af125f2d452a1965a6903e79a796c301088ae2
HYU416 <mvb1031@gmail.com>
2024-11-27 17:21:48 +0900

    音の追加

--- HEW/SoundList.cpp
+++ HEW/SoundList.cpp
@@ -3,10 +3,12 @@
 
 #define MAX_SOUND (8)
 
-#define FILENAME_WALKSE (L"Asset/Sound/title.wav")
-#define FILENAME_WALKSE (L"Asset/Sound/title.wav")
-#define FILENAME_WALKSE (L"Asset/Sound/title.wav")
+#define FILENAME_WALKSE (L"Asset/Sound/walk.wav")
+#define FILENAME_SELECTSE (L"Asset/Sound/select.wav")
+#define FILENAME_DECISIONSE (L"Asset/Sound/decision.wav")
+#define FILENAME_CANCELSE (L"Asset/Sound/cancel.wav")
 #define FILENAME_COMPLETE (L"Asset/Sound/complete.wav")
+#define FILENAME_ARRIVALSE (L"Asset/Sound/star.wav")
 
 #define FILENAME_TITLEBGM (L"Asset/Sound/title.wav")
 #define FILENAME_BATTLEBGM (L"Asset/Sound/battle.wav")
@@ -17,7 +19,12 @@ IXAudio2SourceVoice* g_Source;
 
 void InitSound()
 {
+	g_Sound[SE_WALK] = new CSound(FILENAME_WALKSE);
+	g_Sound[SE_SELECT] = new CSound(FILENAME_SELECTSE);
+	g_Sound[SE_DECISION] = new CSound(FILENAME_DECISIONSE);
+	g_Sound[SE_CANCEL] = new CSound(FILENAME_CANCELSE);
 	g_Sound[SE_COMPLETE] = new CSound(FILENAME_COMPLETE);
+	g_Sound[SE_ARRIVAL] = new CSound(FILENAME_ARRIVALSE);
 
 	g_Sound[BGM_TITLE] = new CSound(FILENAME_TITLEBGM);
 	g_Sound[BGM_BATTLE] = new CSound(FILENAME_BATTLEBGM);
@@ -25,6 +32,7 @@ void InitSound()
 
 void UnInitSound()
 {
+	//âï˙èàóù
 	for (int i = 0; i < MAX_SOUND; i++)
 	{
 		if (!g_Sound[i]) continue;
@@ -35,7 +43,7 @@ void UnInitSound()
 
 
 
-IXAudio2SourceVoice* PlaySound(NAME inName,bool Loop)
+IXAudio2SourceVoice* GetSound(NAME inName,bool Loop)
 {
 	g_Source = g_Sound[inName]->PlayWaveSound(Loop);
 	return g_Source;
