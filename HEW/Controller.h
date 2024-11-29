75a78b1c806eba38df71a6198266e1e61032fcf8
AT12A_YOSHIDA <shiguma665hal@gmail.com>
2024-11-29 17:28:57 +0900

    aa

--- HEW/Controller.h
+++ HEW/Controller.h
@@ -3,11 +3,13 @@
 #include<Xinput.h>
 #pragma comment(lib, "xinput.lib")
 #include"_StructList.h"
+#include "VectorOperator.h"
+
 #define DEADZONE (13000.0f)
 void Controller_Update();
-FLOAT2 CGetLStick();
-FLOAT2 CGetRStick();
-FLOAT2 CGetTrigger();
+CVector2<float> CGetLStick();
+CVector2<float> CGetRStick();
+CVector2<float> CGetTrigger();
 bool CGetButtons(WORD InButton);
 void CSetLeftVibration(int Frame, int Strength);
 void CSetRigthVibration(int Frame, int Strength);
