cca9f91718f3311fe8f09ff8ea7ef6b52c0a3195
HYU416 <mvb1031@gmail.com>
2024-12-04 16:31:52 +0900

    ファイルの変更

--- HEW/File.h
+++ HEW/File.h
@@ -3,7 +3,7 @@
 #include "Battle.h"
 //First(ˆê”ÔÅ‰‚ÉŒÄ‚Ño‚·‚Æ‚«true),WaveSwitch(Wave‚ªØ‚è‘Ö‚í‚é‚Æ‚«‚Étrue),
 //pAllWave(‘SWave”),pWaveNum(Œ»İ‚ÌWave”),pEnemyNum(“G‚Ì”)
-bool InitLoadData(bool First, bool WaveSwitch, int *pPattern,int* pWaveNum , int* pEnemyNum , int* pAllWave);
+bool InitLoadData (bool WaveSwitch, int *pPattern , int* pEnemyNum );
 //pEnemy(\‘¢‘Ì)
 bool EnemyLoadData(int* InCornerCount, int* pSize);
 
