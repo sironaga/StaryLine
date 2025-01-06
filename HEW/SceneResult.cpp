17b5709cb481294946a912655593e2ab4a707e34
sironaga <akinosyoe@gmail.com>
2025-01-06 13:53:31 +0900

    rizaruto

--- HEW/SceneResult.cpp
+++ HEW/SceneResult.cpp
@@ -1,7 +1,30 @@
 #include "SceneResult.h"
 
+ResultGameInfo CSceneResult::ResultGameData;
+
 CSceneResult::CSceneResult()
 {
+	// --- Default
+	m_pHitPoint			= new SpriteEx("Assets/Texture/Result/Result_Helth.png");
+	m_pSummonData		= new SpriteEx("Assets/Texture/Result/Result_Summon.png");
+	m_pSelectStage		= new SpriteEx("Assets/Texture/Result/Result_Stageselect.png");
+
+	if (ResultGameData.bWin)
+	{
+		m_pClearTime	= new SpriteEx("Assets/Texture/Result/Result_Win_Cleartime.png");
+		m_pNextStage	= new SpriteEx("Assets/Texture/Result/Result_Nextstage.png");
+		m_pText			= new SpriteEx("Assets/Texture/Result/Result_Win_Text_top.png");
+		m_pCharacter	= new SpriteEx("Assets/Texture/Result/Result_Win_Linie.png");
+		m_pLighting		= new SpriteEx("Assets/Texture/Result/Result_Win_Light.png");
+	}
+	else
+	{
+		m_pClearTime	= new SpriteEx("Assets/Texture/Result/Result_Lose_Cleartime.png");
+		m_pNextStage	= new SpriteEx("Assets/Texture/Result/Result_Retry.png");
+		m_pText			= new SpriteEx("Assets/Texture/Result/Result_Lose_Text_top.png");
+		m_pCharacter	= new SpriteEx("Assets/Texture/Result/Result_Lose_Linie.png");
+		m_pLighting		= new SpriteEx("Assets/Texture/Result/Result_Lose_Light.png");
+	}
 }
 
 CSceneResult::~CSceneResult()
@@ -14,4 +37,9 @@ void CSceneResult::Update()
 
 void CSceneResult::Draw()
 {
+}
+
+void CSceneResult::InResultData(ResultGameInfo InData)
+{
+	ResultGameData = InData;
 }
--- HEW/SceneResult.h
+++ HEW/SceneResult.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "Scene.h"
+#include"_StructList.h"
+#include"SpriteEx.h"
+#include"Main.h"
 
 class CSceneResult : public CScene
 {
@@ -9,6 +12,20 @@ public:
 	virtual ~CSceneResult();
 	virtual void Update()override;
 	virtual void Draw()override;
+	static void InResultData(ResultGameInfo InData);
 private:
+	SpriteEx* m_pClearTime;
+	SpriteEx* m_pHitPoint;
+	SpriteEx* m_pSummonData;
 
+	SpriteEx* m_pNextStage;
+	SpriteEx* m_pSelectStage;
+	SpriteEx* m_pUI;
+
+	SpriteEx* m_pText;
+	SpriteEx* m_pCharacter;
+	SpriteEx* m_pLighting;
+
+
+	static ResultGameInfo ResultGameData;
 };
