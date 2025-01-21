b6a9c06aec8b6ff2794b7a80b595400908c358fb
AT12A_YOSHIDA <shiguma665hal@gmail.com>
2025-01-21 15:54:26 +0900

    Titleふわふわ、エフェクトマネージャー(スプライトver)完成

--- HEW/SceneTitle.h
+++ HEW/SceneTitle.h
@@ -14,6 +14,7 @@
 #include "Texture.h"
 #include "SpriteEx.h"
 #include "_StructList.h"
+#include "EffectManager_sp.h"
 
 //プロトタイプ宣言
 class CSceneTitle : public CScene
@@ -24,7 +25,7 @@ public:
 	virtual void Update()override;
 	virtual void Draw()override;
 private:
-	void SetResolusion(float wide,float height,bool fullscreen);
+	void SetResolusion(float wide, float height, bool fullscreen);
 private:
 	IXAudio2SourceVoice* g_pSourseTitleBGM;
 	CSoundList* g_TitleSound;
@@ -34,7 +35,7 @@ private:
 private:
 	SpriteEx* m_pTitleLogo;
 	SpriteEx* m_pTitleBack;
-	SpriteEx* m_pLini;
+	SpriteEx* m_pLini[2];
 	SpriteEx* m_pTitleFrame;
 	SpriteEx* m_pTitleUnderbar;
 	SpriteEx* m_pTitleStart[2];
@@ -42,5 +43,17 @@ private:
 	SpriteEx* m_pTitleOption[2];
 	SpriteEx* m_pTitleEnd[2];
 	DirectX::XMFLOAT2 m_SelectPos;
+	DirectX::XMFLOAT2 m_tLiniPos;
 	bool m_bSelected;
+	int m_nAnimCount;
+	int m_nLiniYCount;
+	DirectX::XMFLOAT2 m_tCharaLogoTexPos[2];
+	enum class Effect
+	{
+		Star = 0,
+
+		Max
+	};
+	CEffectManager_sp* m_pEffect[(int)Effect::Max];
+	CEffectManager_sp* m_pEffectArray[10];
 };
